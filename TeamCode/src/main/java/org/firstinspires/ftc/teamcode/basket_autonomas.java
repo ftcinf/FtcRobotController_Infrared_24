e0773876e2d585d4f65d43b7de4b5d85d3c25728
maddie <madeleinehippert@gmail.com>
2024-11-11 20:32:34 -0600

    Organize

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basket_autonomas.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basket_autonomas.java
@@ -3,12 +3,10 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 
 

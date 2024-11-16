b27a8607683e46f23d14bc55dfe9fca4f6af9846
maddie <madeleinehippert@gmail.com>
2024-11-15 20:30:03 -0600

    Organize

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LED.java
@@ -1,80 +1,165 @@
-
 /*
-        Copyright (c) 2024 Alan Smith
-        All rights reserved.
-        Redistribution and use in source and binary forms, with or without modification,
-        are permitted (subject to the limitations in the disclaimer below) provided that
-        the following conditions are met:
-        Redistributions of source code must retain the above copyright notice, this list
-        of conditions and the following disclaimer.
-        Redistributions in binary form must reproduce the above copyright notice, this
-        list of conditions and the following disclaimer in the documentation and/or
-        other materials provided with the distribution.
-        Neither the name of Alan Smith nor the names of its contributors may be used to
-        endorse or promote products derived from this software without specific prior
-        written permission.
-        NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-        LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-        "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-        THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
-        ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-        FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-        DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-        SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-        CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-        TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-        THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-package org.firstinspires.ftc.teamcode;
-
-/*
- * This OpMode illustrates how to use the REV Digital Indicator
+ * Copyright (c) 2018 Craig MacFarlane
  *
- * This is a simple way to add the REV Digital Indicator which has a red and green LED.
- * (and as you may remember, red + green = yellow so when they are both on you can get yellow)
+ * All rights reserved.
  *
- * Why?
- * You can use this to show information to the driver.   For example, green might be that you can
- * pick up more game elements and red would be that you already have the possession limit.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * (subject to the limitations in the disclaimer below) provided that the following conditions are
+ * met:
  *
- * This OpMode assumes that the REV Digital Indicator is setup as 2 Digital Channels named
- * front_led_green and front_led_red. (the green should be the lower of the 2 channels it is plugged
- * into and the red should be the higher)
+ * Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
  *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ * and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
  *
- * You can buy this product here:  https://www.revrobotics.com/rev-31-2010/
+ * Neither the name of Craig MacFarlane nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE. THIS
+ * SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.hardware.LED;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.LED;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
 
-@TeleOp(name = "Concept: RevLED", group = "Concept")
+import java.util.concurrent.TimeUnit;
 
-public class LED extends OpMode {
-    LED frontLED_red;
-    LED frontLED_green;
+/*
+ * This OpMode demonstrates use of the REV Robotics Blinkin LED Driver.
+ * AUTO mode cycles through all of the patterns.
+ * MANUAL mode allows the user to manually change patterns using the
+ * left and right bumpers of a gamepad.
+ *
+ * Configure the driver on a servo port, and name it "blinkin".
+ *
+ * Displays the first pattern upon init.
+ */
+@TeleOp(name="BlinkinExample")
+//@Disabled
+public class Blinkblink extends OpMode {
 
-    @Override
-    public void init() {
-        frontLED_green = hardwareMap.get(LED.class, "left_led");
-        frontLED_red = hardwareMap.get(LED.class, "right_led");
+    /*
+     * Change the pattern every 10 seconds in AUTO mode.
+     */
+    private final static int LED_PERIOD = 10;
+
+    /*
+     * Rate limit gamepad button presses to every 500ms.
+     */
+    private final static int GAMEPAD_LOCKOUT = 500;
+
+    RevBlinkinLedDriver blinkinLedDriver;
+    RevBlinkinLedDriver.BlinkinPattern pattern;
+
+    Telemetry.Item patternName;
+    Telemetry.Item display;
+    DisplayKind displayKind;
+    Deadline ledCycleDeadline;
+    Deadline gamepadRateLimit;
+
+    protected enum DisplayKind {
+        MANUAL,
+        AUTO
     }
 
     @Override
-    public void loop() {
+    public void init()
+    {
+        displayKind = DisplayKind.AUTO;
+
+        blinkinLedDriver = hardwareMap.get(LED.class, "left_led");
+
+        pattern = RevBlinkinLedDriver.BlinkinPattern.RAINBOW_RAINBOW_PALETTE;
+        blinkinLedDriver.setPattern(pattern);
+
+        display = telemetry.addData("Display Kind: ", displayKind.toString());
+        patternName = telemetry.addData("Pattern: ", pattern.toString());
+
+        ledCycleDeadline = new Deadline(LED_PERIOD, TimeUnit.SECONDS);
+        gamepadRateLimit = new Deadline(GAMEPAD_LOCKOUT, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public void loop()
+    {
+        handleGamepad();
+
+        if (displayKind == DisplayKind.AUTO) {
+            doAutoDisplay();
+        } else {
+            /*
+             * MANUAL mode: Nothing to do, setting the pattern as a result of a gamepad event.
+             */
+        }
+    }
+
+    /*
+     * handleGamepad
+     *
+     * Responds to a gamepad button press.  Demonstrates rate limiting for
+     * button presses.  If loop() is called every 10ms and and you don't rate
+     * limit, then any given button press may register as multiple button presses,
+     * which in this application is problematic.
+     *
+     * A: Manual mode, Right bumper displays the next pattern, left bumper displays the previous pattern.
+     * B: Auto mode, pattern cycles, changing every LED_PERIOD seconds.
+     */
+    protected void handleGamepad()
+    {
+        if (!gamepadRateLimit.hasExpired()) {
+            return;
+        }
+
         if (gamepad1.a) {
-            frontLED_red.on();
-        } else {
-            frontLED_red.off();
+            setDisplayKind(DisplayKind.MANUAL);
+            gamepadRateLimit.reset();
+        } else if (gamepad1.b) {
+            setDisplayKind(DisplayKind.AUTO);
+            gamepadRateLimit.reset();
+        } else if ((displayKind == DisplayKind.MANUAL) && (gamepad1.left_bumper)) {
+            pattern = pattern.previous();
+            displayPattern();
+            gamepadRateLimit.reset();
+        } else if ((displayKind == DisplayKind.MANUAL) && (gamepad1.right_bumper)) {
+            pattern = pattern.next();
+            displayPattern();
+            gamepadRateLimit.reset();
         }
-        if (gamepad1.b) {
-            frontLED_green.on();
-        } else {
-            frontLED_green.off();
+    }
+
+    protected void setDisplayKind(DisplayKind displayKind)
+    {
+        this.displayKind = displayKind;
+        display.setValue(displayKind.toString());
+    }
+
+    protected void doAutoDisplay()
+    {
+        if (ledCycleDeadline.hasExpired()) {
+            pattern = pattern.next();
+            displayPattern();
+            ledCycleDeadline.reset();
         }
+    }
+
+    protected void displayPattern()
+    {
+        blinkinLedDriver.setPattern(pattern);
+        patternName.setValue(pattern.toString());
     }
 }
-
